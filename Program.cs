f67a5688079680583c33d800e31bc7ecc767f369
aleksandarbalnozan <balnozan@live.com>
2021-01-21 20:26:19 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AlgorthimicTask
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = { 1, 1, 2, 3, 4, 4, 4, 5, 6, 6 };
+            FindMostFrequent(array);
+        }
+
+        public static void FindMostFrequent(int[] arr)
+        {
+
+            int count;
+            int temp = 0;
+            int mostFrequent = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                count = 0;
+                for (int j = 0; j < arr.Length; j++)
+                {
+
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count > temp)
+                {
+                    temp = count;
+                    mostFrequent = arr[i];
+                }
+            }
+            Console.WriteLine(mostFrequent);
+        }
+    }
+}
